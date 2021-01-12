df1b5c3496784ce67a86e4c77e3554b4c34b2425
MikHut <mhutchinson@sagarobotics.com>
2021-01-12 15:04:47 +0000

    tidying and general config

--- scripts/ntripclient.py
+++ scripts/ntripclient.py
@@ -12,10 +12,12 @@ from threading import Thread
 from std_msgs.msg import String
 
 class ntripconnect(Thread):
+
     def __init__(self, ntc):
         super(ntripconnect, self).__init__()
         self.ntc = ntc
         self.stop = False
+
 
     def run(self):
         headers = {
@@ -27,7 +29,7 @@ class ntripconnect(Thread):
         connection = HTTPConnection(self.ntc.ntrip_server)
         now = datetime.datetime.utcnow()
         nmeadata = self.ntc.nmea_gga % (now.hour, now.minute, now.second)
-        
+
         csum = 0
         for c in nmeadata:
                # XOR'ing value of csum against the next char in line
@@ -36,10 +38,10 @@ class ntripconnect(Thread):
                   csum ^= ord(c)
 
         #convert hex characters to upper case
-        csum = hex(csum).upper() 
+        csum = hex(csum).upper()
 
         #add 0x0 if checksum value is less than 0x10
-        if len(csum)==3:	
+        if len(csum)==3:
             csum='0'+csum[2]
         else:
             csum=csum[2:4]
@@ -47,7 +49,7 @@ class ntripconnect(Thread):
         nmeastring = '$'+nmeadata+'*'+csum
 
         connection.request('GET', '/'+self.ntc.ntrip_stream,nmeastring,headers)
-        
+
         response = connection.getresponse()
         if response.status != 200: raise Exception("blah")
         buf = ""
@@ -59,7 +61,7 @@ class ntripconnect(Thread):
             l1 = ord(response.read(1))
             l2 = ord(response.read(1))
             pkt_len = ((l1&0x3)<<8)+l2
-    
+
             pkt = response.read(pkt_len)
             parity = response.read(3)
             if len(pkt) != pkt_len:
@@ -68,35 +70,37 @@ class ntripconnect(Thread):
             rmsg.header.seq += 1
             rmsg.header.stamp = rospy.get_rostime()
             rmsg.data = data + chr(l1) + chr(l2) + pkt + parity
-            self.ntc.pub.publish(rmsg)
-            # self.ntc.pu.publish("hi")
+            self.ntc.rtcm_pub.publish(rmsg)
         connection.close()
 
 
 class ntripclient:
+
     def __init__(self):
         rospy.init_node('ntripclient', anonymous=True)
-
-        self.rtcm_topic = rospy.get_param('~rtcm_topic','/rtcm')
-        self.nmea_topic = rospy.get_param('~nmea_topic', 'nmea')
 
         self.ntrip_server = rospy.get_param('~ntrip_server')
         self.ntrip_user = rospy.get_param('~ntrip_user')
         self.ntrip_pass = rospy.get_param('~ntrip_pass')
         self.ntrip_stream = rospy.get_param('~ntrip_stream')
+
+        # NMEA GGA data is needed to send to NTRIP server to keep connection
+        # Should replace with real GGA msg from our fix or nmea msg
         self.nmea_gga = rospy.get_param('~nmea_gga')
 
-        self.pub = rospy.Publisher(self.rtcm_topic, RTCM, queue_size=10)
-        self.pu = rospy.Publisher("chatter", String, queue_size=10)
+        self.rtcm_pub = rospy.Publisher('rtcm', RTCM, queue_size=10)
 
         self.connection = None
         self.connection = ntripconnect(self)
         self.connection.start()
 
+
     def run(self):
         rospy.spin()
         if self.connection is not None:
-            self.connection.stop = True
+          self.connection.stop = True
+
+
 
 if __name__ == '__main__':
     c = ntripclient()
